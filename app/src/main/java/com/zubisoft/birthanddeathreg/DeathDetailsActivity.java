d1b4a39602edfff684a3d646e08b77ef718ef534
victor <abraham.nnu@gmail.com>
2021-04-23 13:42:47 +0100

    update

---
+++ app/src/main/java/com/zubisoft/birthanddeathreg/DeathDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.zubisoft.birthanddeathreg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeathDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_death_details);
+    }
+}
